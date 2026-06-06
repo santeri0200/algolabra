c74259a6d3a19322896a7db928699d45a5d4c5a9
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-06-06 18:50:40 +0300

    [CODE] Add QOI/PNG switch to the entry mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,27 @@
 #include <iostream>
 
 #include "qoi.cpp"
+#include "png.cpp"
 
 int entry(int argc, char const *argv[]) {
-  if (argc != 2) {
+  if (argc != 3) {
     std::cerr << "Invalid amount of arguments!\n";
 
     return -1;
   }
 
-  const char *mode = argv[1];
-  const char *source = argv[2];
+  const char *type = argv[1];
+  const char *mode = argv[2];
+  const char *source = argv[3];
 
   if (!std::filesystem::exists(source)) {
     return -1;
   }
 
-  if (strcmp(mode, "decode") == 0) {
-    return decode(source);
+  if (strcmp(type, "qoi") == 0 && strcmp(mode, "decode") == 0) {
+    return qoi::decode(source);
+  } else if (strcmp(type, "png") == 0 && strcmp(mode, "decode") == 0) {
+    return png::decode(source);
   }
 
   return 0;
