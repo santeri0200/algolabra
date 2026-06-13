95b634a63d1a7315766f9b0e8e335f4d10b6640e
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-06-13 15:21:09 +0300

    [CODE] Merge both encoding paths

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int entry(int argc, char const *argv[]) {
     return qoi::decode(source);
   } else if (strcmp(type, "png") == 0 && strcmp(mode, "decode") == 0) {
     return png::decode(source);
-  } else if (strcmp(type, "qoi") == 0 && strcmp(mode, "encode") == 0) {
+  } else if (strcmp(mode, "encode") == 0) {
     if (argc < 5) {
       std::cerr << "Invalid amount of arguments!" << argc << "\n";
 
@@ -41,39 +41,18 @@ int entry(int argc, char const *argv[]) {
     }
 
     std::vector<uint8_t>output;
-    if (qoi::encode(image, output) < 0) {
-      std::cerr << "Failed to encode!\n";
-      return -1;
-    }
-    
-    std::ofstream outFile(dist, std::ios::binary);
-    if (outFile.is_open()) {
-        outFile.write((const char *)output.data(), output.size());
-        outFile.close();
-    } else {
-      std::cerr << "Failed to write the file\n";
-    }
-  } else if (strcmp(type, "png") == 0 && strcmp(mode, "encode") == 0) {
-    if (argc < 5) {
-      std::cerr << "Invalid amount of arguments!" << argc << "\n";
-
-      return -1;
+    if (strcmp(type, "qoi") == 0) {
+      if (qoi::encode(image, output) < 0) {
+        std::cerr << "Failed to encode!\n";
+        return -1;
+      }
+    } else if (strcmp(type, "png") == 0) {
+      if (png::encode(image, output) < 0) {
+        std::cerr << "Failed to encode!\n";
+        return -1;
+      }
     }
 
-    const char *dist = argv[4];
-
-    Image image = {};
-    if (bmp::decode(source, image) < 0) {
-      std::cerr << "Failed to process the bmp file!\n";
-      return -1;
-    }
-
-    std::vector<uint8_t>output;
-    if (png::encode(image, output) < 0) {
-      std::cerr << "Failed to encode!\n";
-      return -1;
-    }
-    
     std::ofstream outFile(dist, std::ios::binary);
     if (outFile.is_open()) {
         outFile.write((const char *)output.data(), output.size());
