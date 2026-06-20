2fbb01ba519302be5db844f8e88e3541068f7c9f
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-06-20 14:23:03 +0300

    [TEST] Rework tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,40 +1,115 @@
 #include <gtest/gtest.h>
 #include "../src/main.cpp"
 
-TEST(QOI_decode, ErrorOnInvalidArguments) {
-	EXPECT_EQ(entry(0, nullptr), -1);
-}
-
-TEST(QOI_decode, ErrorOnInvalidFilePath) {
-	const int ARGC = 3;
-	const char* ARGV[] = { "algolab", "qoi", "decode", "data/output.txt" };
+TEST(BMP_decode, FailOnInvalidBMP) {
+	const int ARGC = 5;
+	const char* ARGV[ARGC] = { "algolab", "bmp", "decode", "data/fake.bmp", "out.png" };
 
 	EXPECT_EQ(entry(ARGC, ARGV), -1);
 }
 
-TEST(QOI_decode, SucceedOnValidFilePath) {
-	const int ARGC = 3;
-	const char* ARGV[] = { "algolab", "qoi", "decode", "data/input.txt" };
+TEST(BMP_decode, SucceedOnValid1BPPBMP) {
+	const int ARGC = 5;
+	const char* ARGV[ARGC] = { "algolab", "bmp", "decode", "data/test.bmp", "out.png" };
 
 	EXPECT_EQ(entry(ARGC, ARGV), 0);
 }
 
-TEST(PNG_decode, ErrorOnInvalidArguments) {
-	EXPECT_EQ(entry(0, nullptr), -1);
-}
-
-TEST(PNG_decode, ErrorOnInvalidFilePath) {
-	const int ARGC = 3;
-	const char* ARGV[] = { "algolab", "png", "decode", "data/output.txt" };
-
-	EXPECT_EQ(entry(ARGC, ARGV), -1);
-}
-
-TEST(PNG_decode, SucceedOnValidFilePath) {
-	const int ARGC = 3;
-	const char* ARGV[] = { "algolab", "png", "decode", "data/input.txt" };
+TEST(BMP_decode, SucceedOnValid24BPPBMP) {
+	const int ARGC = 5;
+	const char* ARGV[ARGC] = { "algolab", "bmp", "decode", "data/test3.bmp", "out.png" };
 
 	EXPECT_EQ(entry(ARGC, ARGV), 0);
+}
+
+TEST(PNG_encode, SucceedOnValidFilePath) {
+	const int ARGC = 5;
+	const char* ARGV[ARGC] = { "algolab", "png", "encode", "data/test.bmp", "out.png" };
+
+	EXPECT_EQ(entry(ARGC, ARGV), 0);
+}
+
+TEST(QOI_encode, SucceedOnValidFilePath) {
+	const int ARGC = 5;
+	const char* ARGV[ARGC] = { "algolab", "qoi", "encode", "data/test.bmp", "out.png" };
+
+	EXPECT_EQ(entry(ARGC, ARGV), 0);
+}
+
+TEST(PNG_encode, SucceedOnCubeBMP) {
+	const int ARGC = 5;
+	const char* ARGV[ARGC] = { "algolab", "png", "encode", "data/dice.bmp", "out.png" };
+
+	EXPECT_EQ(entry(ARGC, ARGV), 0);
+}
+
+TEST(QOI_encode, SucceedOnCubeBMP) {
+	const int ARGC = 5;
+	const char* ARGV[ARGC] = { "algolab", "qoi", "encode", "data/dice.bmp", "out.png" };
+
+	EXPECT_EQ(entry(ARGC, ARGV), 0);
+}
+
+TEST(PNG_encode_decode, CanProduceOriginal) {
+	Image image = {};
+	const char* source = "data/test.bmp";
+	EXPECT_EQ(source_exists(source), 0);
+	EXPECT_EQ(bmp::decode(source, image), 0);
+
+	std::vector<uint8_t>png;
+	EXPECT_GE(png::encode(image, png), 0);
+
+	const char* out = "tests/test.png";
+	std::ofstream pngOutFile(out, std::ios::binary);
+	EXPECT_EQ(pngOutFile.is_open(), true);
+	pngOutFile.write((const char *)png.data(), png.size());
+	pngOutFile.close();
+	EXPECT_EQ(source_exists(out), 0);
+
+	Image output_image = {};
+	EXPECT_EQ(png::decode(out, output_image), 0);
+
+	EXPECT_EQ(image.height, output_image.height);
+	EXPECT_EQ(image.width, output_image.width);
+
+	int diff = 0;
+	for (int i = 0; i < image.data.size(); ++i) {
+		if (image.data[i] != output_image.data[i]) diff++;
+		EXPECT_EQ(image.data[i], output_image.data[i]);
+	}
+
+	EXPECT_EQ(diff, 0);
+}
+
+TEST(QOI_encode_decode, CanProduceOriginal) {
+  Image image = {};
+  const char* source = "data/test.bmp";
+  EXPECT_EQ(source_exists(source), 0);
+	EXPECT_EQ(bmp::decode(source, image), 0);
+
+  std::vector<uint8_t>qoi;
+	EXPECT_GE(qoi::encode(image, qoi), 0);
+
+	const char* out = "tests/test.qoi";
+  std::ofstream qoiOutFile(out, std::ios::binary);
+  EXPECT_EQ(qoiOutFile.is_open(), true);
+  qoiOutFile.write((const char *)qoi.data(), qoi.size());
+  qoiOutFile.close();
+  EXPECT_EQ(source_exists(out), 0);
+
+  Image output_image = {};
+  EXPECT_EQ(qoi::decode(out, output_image), 0);
+
+  EXPECT_EQ(image.height, output_image.height);
+  EXPECT_EQ(image.width, output_image.width);
+
+  int diff = 0;
+  for (int i = 0; i < image.data.size(); ++i) {
+  	if (image.data[i] != output_image.data[i]) diff++;
+  	EXPECT_EQ(image.data[i], output_image.data[i]);
+  }
+
+  EXPECT_EQ(diff, 0);
 }
 
 #ifdef USE_GTEST_MAIN
