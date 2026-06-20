5cad55d90b835c5f6fec589d0bb9dde07051d328
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-06-20 14:58:17 +0300

    [CODE] Add comparison feature

--- src/main.cpp
+++ src/main.cpp
@@ -74,6 +74,63 @@ int entry(int argc, char const *argv[]) {
     } else {
       std::cerr << "Failed to write the file\n";
     }
+  } else  if (strcmp(mode, "compare") == 0) {
+    Image image = {};
+    if (bmp::decode(source, image) < 0) {
+      std::cerr << "Failed to process the bmp file!\n";
+      return -1;
+    }
+
+    if (strcmp(type, "both") == 0) {
+      std::vector<uint8_t>qoiOutput;
+      if (qoi::encode(image, qoiOutput) < 0) {
+        std::cerr << "Failed to encode qoi!\n";
+        return -1;
+      }
+
+      std::vector<uint8_t>pngOutput;
+      if (png::encode(image, pngOutput) < 0) {
+        std::cerr << "Failed to encode png!\n";
+        return -1;
+      }
+
+      size_t originalSize = std::filesystem::file_size(source);
+      size_t qoiSize = qoiOutput.size();
+      size_t pngSize = pngOutput.size();
+
+      std::cout << "width: " << image.width << ", height: " << image.width << ", pixels: " << image.data.size() / 4 << '\n';
+      std::cout << "original size: " << originalSize << " bytes\n";
+      std::cout << "qoi size: " << qoiSize << " bytes, compression ratio: " << (float)originalSize / (float)qoiSize << ":1\n";
+      std::cout << "png size: " << pngSize << " bytes, compression ratio: " << (float)originalSize / (float)pngSize << ":1\n";
+
+      std::cout << "\nqoi output is the " << (float)qoiSize / (float)pngSize * 100.0 << "% size of png output.\n";
+    } else if (strcmp(type, "qoi") == 0) {
+      std::vector<uint8_t>qoiOutput;
+      if (qoi::encode(image, qoiOutput) < 0) {
+        std::cerr << "Failed to encode qoi!\n";
+        return -1;
+      }
+
+      size_t originalSize = std::filesystem::file_size(source);
+      size_t qoiSize = qoiOutput.size();
+
+      std::cout << "width: " << image.width << ", height: " << image.width << ", pixels: " << image.data.size() / 4 << '\n';
+      std::cout << "original size: " << originalSize << " bytes\n";
+      std::cout << "qoi size: " << qoiSize << " bytes, compression ratio: " << (float)originalSize / (float)qoiSize << ":1\n";
+    } else if (strcmp(type, "png") == 0) {
+      std::vector<uint8_t>pngOutput;
+      if (png::encode(image, pngOutput) < 0) {
+        std::cerr << "Failed to encode png!\n";
+        return -1;
+      }
+
+      size_t originalSize = std::filesystem::file_size(source);
+      size_t pngSize = pngOutput.size();
+
+      std::cout << "width: " << image.width << ", height: " << image.width << ", pixels: " << image.data.size() / 4 << '\n';
+      std::cout << "original size: " << originalSize << " bytes\n";
+      std::cout << "png size: " << pngSize << " bytes, compression ratio: " << (float)originalSize / (float)pngSize << ":1\n";
+    }
   }
 
   return 0;
